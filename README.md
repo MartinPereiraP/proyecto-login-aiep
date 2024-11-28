38f90346d8a94e645ac82b81609d02478744c237
Martin Pereira <martinpereirap@gmail.com>
2024-11-28 10:36:15 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Proyecto Login PHP
+
+## Modulo Programación Segura
+
+## [GitHub](app/Models/ActivityLog.php)
+
+- Conectar a la base de datos usna
+- usar github
+- crear login y validaciones
+- en el nav se debe crear un boton login y register a la derecha del nav tarea para el sabado
+- cambiar datos al navbar
+  
+```php
+├-- app/
+|       ├-- Controllers/
+|       ├-- Models/
+|       └-- Traits/
+├-- config/
+|       └-- database.php
+├-- .gitignore
+├-- composer.json
+├-- package.json
+```
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// public/index.php
+
+// Establecer la configuración de errores de PHP para producción
+// Para que los errores se muestren solo en el registro de errores
+// y no en la salida al usuario.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+
+// Asegurarse de que solo se acceda al script desde el servidor web
+if ($_SERVER['REMOTE_ADDR'] !== $_SERVER['SERVER_ADDR']) {
+    http_response_code(403);
+    exit;
+}
+define('BASE_PATH', dirname(__DIR__)); // Define la ruta base del proyecto
+
+// Cargar el autoload de Composer para cargar automáticamente todas las clases
+require_once BASE_PATH . '/vendor/autoload.php';
+// Cargar el autoload de Composer para cargar automáticamente todas las clases.
+require_once __DIR__ . '/../bootstrap/app.php';
+require_once __DIR__ . '/../routes/web.php';
